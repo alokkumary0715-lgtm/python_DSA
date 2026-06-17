3acb6d94d3aa561a75cdbdf2c435c5b7b38cb8be
alokkumary0715-lgtm <alokkumary0715@gmail.com>
2026-06-17 11:47:31 +0530

    patterns

---
+++ pattern/patterns.py
@@ -0,0 +1,22 @@
+# for i in range(1,6):   # this will crete your row 
+#     for j in range (1,6): # this will crete your column
+#         print(j,end=" ")
+#     print()
+
+
+# for i in range(6,0,-1):
+#     for j in range(1,6):
+#         print(i,end=" ")
+#     print()
+
+
+
+# for i in range (1,6):
+#     for j in range(1,i+1):
+#         print("*", end=" ")
+#     print()
+
+for i in range (0,6):
+    for j in range (i,0,-1):
+        print(j,end=" ")
+    print() 
