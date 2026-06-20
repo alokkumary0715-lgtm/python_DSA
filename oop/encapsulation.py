d5cf28b7769f9bd7560d41893590ccb0c2eeeb34
alokkumary0715-lgtm <alokkumary0715@gmail.com>
2026-06-20 12:07:32 +0530

    example enc

--- oop/encapsulation.py
+++ oop/encapsulation.py
@@ -2,30 +2,30 @@
 user interact through method instead directly changing data"""
 
 #without encapsulation
-# class employee:
-#     def __init__(self):
-#         self.salary = 6000
+class employee:
+    def __init__(self):
+        self.salary = 6000
 
-# emp = employee()
-# emp.salary = -1000  # here i have changed the salry 
-# print(emp.salary) 
+emp = employee()
+emp.salary = -1000  # here i have changed the salry 
+print(emp.salary) 
 
 
-#with encapsulation
-# class employee:
-#     def __init__(self):
-#         self.__salary = 6000   #__this to make private
+with encapsulation
+class employee:
+    def __init__(self):
+        self.__salary = 6000   #__this to make private
 
-#     def set_salary(self,salary):
-#         if salary>0:
-#             self.__salary=salary
+    def set_salary(self,salary):
+        if salary>0:
+            self.__salary=salary
 
-#     def get_slary(self):
-#         return self.__salary
+    def get_slary(self):
+        return self.__salary
     
-# emp = employee()
-# emp.set_salary(-100)
-# print(emp.get_slary())
+emp = employee()
+emp.set_salary(-100)
+print(emp.get_slary())
 
 class employee:
     def __init__(self):
@@ -43,5 +43,28 @@ class employee:
 emp = employee()
 result = emp.set_salary(-100)
 if result:
-    print(result)
+    print(result)          #here we use getter
 print(emp.get_slary())
+
+
+#example bank
+class bank:
+    def __init__(self,balance):
+        self.__balance = balance
+
+    def deposit(self,amount):
+        self.__balance += amount
+
+    def withdraw(self,amount):
+        if amount <= self.__balance:
+            self.__balance -= amount
+            return self.__balance
+        else:
+            print("insufficient balance")
+
+    def show_balance(self):
+        print("balance",self.__balance)
+
+acc1 = bank(1000)
+result=acc1.withdraw(200)
+print(result)
