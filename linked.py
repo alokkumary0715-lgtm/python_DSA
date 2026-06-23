e82d7f33017c840172dbc8267b10917d8df8f684
alokkumary0715-lgtm <alokkumary0715@gmail.com>
2026-06-23 11:47:22 +0530

    lifecycle

--- linked.py
+++ linked.py
@@ -1,3 +1,24 @@
+
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+# Create nodes
+node1 = ListNode(1)
+node2 = ListNode(2)
+node3 = ListNode(3)
+node4 = ListNode(4)
+
+# Connect nodes
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+# Create cycle
+node4.next = node2
+
 class solution:
     def cycle(self,head):
         temp = head
@@ -9,3 +30,5 @@ class solution:
             temp= temp.next
         return False
             
+obj = solution()
+print(obj.cycle(node1))
