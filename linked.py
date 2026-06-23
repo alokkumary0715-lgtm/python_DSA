c9fe00d68b5c75a3af5d27c4b782d38c178d54ff
alokkumary0715-lgtm <alokkumary0715@gmail.com>
2026-06-23 11:41:52 +0530

    linked list cycle

---
+++ linked.py
@@ -0,0 +1,11 @@
+class solution:
+    def cycle(self,head):
+        temp = head
+        my_set = set()
+        while temp is not None:
+            if temp in my_set:
+                return True
+            my_set.add(temp)
+            temp= temp.next
+        return False
+            
